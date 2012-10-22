eae7c2de4bebd6ccdf0d5839bb995e6dcec552a4
mykebuntu <mykebuntu@mykebuntu-VirtualBox.(none)>
2012-10-22 04:40:29 -0700

    Tabbed hosting plan navigation started

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,4 +1,8 @@
 (function($) {
     $(function(){
+        $('.hosting-plan-details a').click(function (e) {
+          e.preventDefault();
+          $(this).tab('show');
+        })
     });
 })(jQuery);
